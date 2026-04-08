7a5c9ea9715b401dd985a05d48538450ac51335f
aym.kopi <justineacun@gmail.com>
2026-04-09 07:26:07 +0800

    initial commit using expo sdk55 + heroui

---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import { HeroUINativeProvider } from "heroui-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "../../global.css";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Stack />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
+}
